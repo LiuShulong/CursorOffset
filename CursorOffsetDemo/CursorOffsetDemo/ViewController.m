90c742e84bed775e89e3dde2d896a3478eee989f
LiuShulong <bigfish_liu@126.com>
2015-07-25 20:05:56 +0800

    release 1.0.1

--- CursorOffsetDemo/CursorOffsetDemo/ViewController.m
+++ CursorOffsetDemo/CursorOffsetDemo/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "UITextField+CalculateCursorOffset.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraint;
 
 @end
 
@@ -23,5 +27,18 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - click
+
+- (IBAction)click:(id)sender {
+    self.constraint.constant = [self.textField cursorOffset] + self.textField.frame.origin.x;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.textField resignFirstResponder];
+}
+
+
 
 @end
