f894e80cab3eca7ba165c0d0afdbcc55d988ccfe
LiuShulong <bigfish_liu@126.com>
2015-07-25 19:43:26 +0800

    Release 1.0.0

---
+++ CursorOffsetDemo/CursorOffsetDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CursorOffsetDemo
+//
+//  Created by LiuShulong on 15/7/25.
+//  Copyright © 2015年 LiuShulong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CursorOffsetDemo/CursorOffsetDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CursorOffsetDemo
+//
+//  Created by LiuShulong on 15/7/25.
+//  Copyright © 2015年 LiuShulong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
